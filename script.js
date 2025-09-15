f96fe3cdc27d4c67b324dcf6452205fd740ad239
SMARTIED <smartied.79@gmail.com>
2025-09-15 14:23:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const links = document.querySelectorAll('.nav-link');
-const carousel = document.querySelector('.carousel');
-const slides = document.querySelectorAll('.carousel-slide');
 
-links.forEach((link, index) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    carousel.style.transform = `translateX(-${index * 100}%)`;
-
-    links.forEach(l => l.classList.remove('active'));
-    link.classList.add('active');
-  });
-});
